Android-Piepaint/c001apk-flutter

commit 1c312d93feecd68867d66b1fa27acca9e6a0575f

feat: hide 'Theme Color' setting when 'Dynamic Theme' is enabled

lib/pages/settings/settings_page.dart
@@ -122,19 +122,19 @@ class _SettingsPageState extends State<SettingsPage>
             defaultValue: true,
             forceAppUpdate: true,
           ),
-          DropDownMenuItem(
-            icon: Icons.format_color_fill,
-            title: 'Theme Color',
-            boxKey: SettingsBoxKey.staticColor,
-            items: Constants.themeType
-                .map((type) => DropdownMenuItem<int>(
-                      enabled: !GStorage.useMaterial,
-                      value: Constants.themeType.indexOf(type),
-                      child: Text(type),
-                    ))
-                .toList(),
-            forceAppUpdate: true,
-          ),
+          if (!GStorage.useMaterial)
+            DropDownMenuItem(
+              icon: Icons.format_color_fill,
+              title: 'Theme Color',
+              boxKey: SettingsBoxKey.staticColor,
+              items: Constants.themeType
+                  .map((type) => DropdownMenuItem<int>(
+                        value: Constants.themeType.indexOf(type),
+                        child: Text(type),
+                      ))
+                  .toList(),
+              forceAppUpdate: true,
+            ),
           const DropDownMenuItem(
             icon: Icons.dark_mode_outlined,
             title: 'Dark Mode',

commit 8e059e703c6a6781c7db1483a3d53f511c0356a8

fix: resolve a regression in theme settings
This commit resolves a regression introduced in commit 1c312d9
that causes exception when toggling the 'Dynamic Theme' setting
on and off.

lib/components/settings/drop_down_menu_item.dart
@@ -40,7 +40,7 @@ class _DropDownMenuItemState extends State<DropDownMenuItem> {
       trailing: DropdownButton<int>(
         value: value,
         onChanged: (int? newValue) {
-          if (newValue != null) {
+          if (newValue != null && newValue != value) {
             GStorage.settings.put(widget.boxKey, newValue);
             setState(() => value = newValue);
             if (widget.forceAppUpdate) {

lib/pages/settings/settings_page.dart
@@ -122,8 +122,9 @@ class _SettingsPageState extends State<SettingsPage>
             defaultValue: true,
             forceAppUpdate: true,
           ),
-          if (!GStorage.useMaterial)
-            DropDownMenuItem(
+          Visibility(
+            visible: !GStorage.useMaterial,
+            child: DropDownMenuItem(
               icon: Icons.format_color_fill,
               title: 'Theme Color',
               boxKey: SettingsBoxKey.staticColor,
@@ -135,6 +136,7 @@ class _SettingsPageState extends State<SettingsPage>
                   .toList(),
               forceAppUpdate: true,
             ),
+          ),
           const DropDownMenuItem(
             icon: Icons.dark_mode_outlined,
             title: 'Dark Mode',