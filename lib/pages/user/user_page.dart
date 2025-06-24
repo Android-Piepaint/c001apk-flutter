52b8a1cbd6bf8c0906405c498417fa4b0e358b4b
Integral <integral@member.fsf.org>
2025-06-24 21:43:50 +0800

    refactor: eliminate indent package dependency from UserPage

--- lib/pages/user/user_page.dart
+++ lib/pages/user/user_page.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
-import 'package:indent/indent.dart';
 import 'package:intl/intl.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -281,16 +280,11 @@ class _UserPageState extends State<UserPage> {
                                   maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
                                 ),
-                                content: Text(
-                                  '''
-                                      |UID: ${user.uid}\n
-                                      |等级: Lv.${user.level}\n
-                                      |性别: ${user.gender == 1 ? '男' : user.gender == 0 ? '女' : '未知'}\n
-                                      |注册时长: ${((DateTime.now().microsecondsSinceEpoch ~/ 10e5 - user.regdate!) ~/ 24 ~/ 3600)} 天\n
-                                      |注册时间: ${DateFormat('yyyy年MM月dd日 HH:mm:ss').format(DateTime.fromMillisecondsSinceEpoch(user.regdate * 1000))}
-                                      '''
-                                      .trimMargin(),
-                                ),
+                                content: Text('UID: ${user.uid}\n'
+                                    '等级: Lv.${user.level}\n'
+                                    '性别: ${user.gender == 1 ? '男' : user.gender == 0 ? '女' : '未知'}\n'
+                                    '注册时长: ${((DateTime.now().microsecondsSinceEpoch ~/ 10e5 - user.regdate!) ~/ 24 ~/ 3600)} 天\n'
+                                    '注册时间: ${DateFormat('yyyy年MM月dd日 HH:mm:ss').format(DateTime.fromMillisecondsSinceEpoch(user.regdate * 1000))}'),
                               );
                             },
                           );
